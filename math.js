1738e3e9d1c1743832f20f68795bb71fe2cb70fe
Brian Lin <brian870929@gmail.com>
2023-02-23 15:12:18 +0800

    first commit

---
+++ math.js
@@ -0,0 +1,13 @@
+module.exports.add = (x, y) => x + y;
+
+module.exports.PI = 3.14159;
+
+module.exports.square = x => x * x;
+
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+
+// module.exports = math;
